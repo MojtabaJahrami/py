b932aae2573c221a80eb47c6e310b3d2609aab4f
MojtabaJahrami <85936611+MojtabaJahrami@users.noreply.github.com>
2024-01-28 11:10:51 +0330

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,30 @@
+#دیکشنری (dict)
+myDict = {
+    'name': 'hamid',
+    'family': 'hamidi',
+    'score': 20,
+    'passed': True,
+}
+
+#لیست (list)
+
+myList1 = ['ali', 'mohammad', 'reza', 'mahmood', 'reza']
+
+#مجموعه (set)
+mySet = {'ali', 'ahmad', 'hamid', 'reza', 'hamid'}
+
+
+#تاپل (tuple)
+myTuple1 = ('ali', 'hassan', 'hossein', 'mahmood', 'majid', 'ali')
+
+
+#حلقه for
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print("Number ", i);
+
+#if
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
